6e0a99374ed1e341c1cd1eae89cc49cd7bb002d3
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2022-01-09 20:50:13 +0300

    Added increment / decrement functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import React, {useState} from 'react';
+
+
 function App() {
+  const [count, setCount] = useState(1)
+  const [value, setValue] = useState('some text')
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  function decrement() {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
+      <h1>{count}</h1>
+      <h2>{value}</h2>
+      <input
+        type='text'
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Increment</button>
     </div>
   );
 }
