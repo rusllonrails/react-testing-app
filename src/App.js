a7b7fda067f6730895f419a19ae351f39a7ea553
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2022-01-11 23:34:29 +0300

    Implemented ability to sort collections

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, {useState, useRef} from 'react';
 import PostList from './components/PostList';
 import InputWithUseRef from './components/UI/input/InputWithUseRef'
+import MySelect from './components/UI/select/MySelect'
 import PostForm from './components/PostForm'
 import './styles/App.css';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, name: 'Javascript', desc: 'Some text 1'},
-    {id: 2, name: 'Ruby', desc: 'Some text 2'},
-    {id: 3, name: 'SQL', desc: 'Some text 3'}
+    {id: 1, name: 'Alex', desc: 'Yellow'},
+    {id: 2, name: 'Ciara', desc: 'Black'},
+    {id: 3, name: 'Rango', desc: 'Absolute'}
   ])
+
+  const [selectedSort, setSelectedSort] = useState('name')
 
   {/* const bodyInputRef = useRef() */}
 
@@ -21,9 +24,25 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortBy = (sort) => {
+    setSelectedSort(sort)
+    const sorted = [...posts].sort((a,b) => a[sort].localeCompare(b[sort]))
+    setPosts(sorted)
+  }
+
   return (
     <div className="App">
       {/* <InputWithUseRef ref={bodyInputRef} type='text'placeholder='new post (useRef)' onChange={(e) => console.log(bodyInputRef.current.value)} /> */}
+
+      <MySelect
+        value={selectedSort}
+        onChange={sortBy}
+        options={[
+          {value: 'name', name: 'by name'},
+          {value: 'desc', name: 'by body'}
+        ]}
+        defaultValue={'name'}
+       />
       <PostForm create={createPost} />
 
       {posts.length > 0
---
+++ src/components/UI/select/MySelect.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MySelect = ({options, defaultValue, value, onChange}) => {
+  return (
+    <select value={value} onChange={(event) => onChange(event.target.value)}>
+      {options.map(option =>
+        <option key={option.value} value={option.value}>{option.name}</option>
+      )}
+    </select>
+  )
+}
+
+export default MySelect;
